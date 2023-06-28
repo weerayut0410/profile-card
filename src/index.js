b09885444157120e8afd98f17724019d34cc4995
weerayut0410 <weerayut.la.65@ubu.ac.th>
2023-06-28 15:18:43 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <div className='card'>
+        <Avatar />
+        <dvi className ='data'>
+          <Intro/>
+          <SkillList/>
+        </dvi>
+      </div>
+    </>
+  );
+  }
+
+  function SkillList () {
+    return (
+      <div className="skill-list">
+        <Skill skill = "sleep" emoji = "✌️" color = "brown" />
+        <Skill skill = "play" emoji = "🦾" color = "yellow" />
+        <Skill skill = "learning" emoji = "💀" color = "white" /> 
+        <Skill skill = "work" emoji = "☠️" color = "silver" />
+      </div>
+    );
+  }
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>นายวีระยุทธ ลักษ์ศิลป์</h1>
+      <p>
+        นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์ 
+        มหาวิทยาลัยอุบลราชธานี
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src="avatar.jpg" alt='My Avatar' />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
